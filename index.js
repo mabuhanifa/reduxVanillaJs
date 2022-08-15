978eee562b1704862a1c92b5ad62d9ac8b06d4ea
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-15 16:24:17 +0600

    changed

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 const addCounter = document.getElementById("addCounter");
 const classd = "increment bg-indigo-400 text-white px-3 py-2 rounded shadow";
-const inner = `
-  <div id="counter" class="text-2xl font-semibold"></div>
-  <button class="${classd}" > 
-    Increment
-  </button>
-  <button onclick="loadP()"  class="decrement bg-red-400 text-white px-3 py-2 rounded shadow" >
-    Decrement
-  </button>
-`;
-
 // initial state
 const initialState = [
   {
@@ -28,7 +18,7 @@ function counterReducer(state = initialState, action) {
   if (type === "increment") {
     return [...state, (state[id].value = state[id].value + payload)];
   } else if (type === "decrement") {
-    return [...state, state[0].value--];
+    return [...state, (state[id].value = state[id].value - payload)];
   } else if (type === "addCounter") {
     return [...state, payload];
   } else {
@@ -63,12 +53,24 @@ incrementEl.addEventListener("click", () => {
 decrementEl.addEventListener("click", () => {
   store.dispatch({
     type: "decrement",
+    id: 0,
+    payload: 2,
   });
 });
 
 function myFunction() {
   const node = document.createElement("div");
-
+  const state = store.getState();
+  const inner = `
+  <div class="text-2xl font-semibold"></div>
+  <h2>${state[0].value}</h2>
+  <button class="${classd}" > 
+    Increment
+  </button>
+  <button onclick="loadP()"  class="decrement bg-red-400 text-white px-3 py-2 rounded shadow" >
+    Decrement
+  </button>
+`;
   node.innerHTML = inner;
 
   document.getElementById("container").appendChild(node);
@@ -87,6 +89,4 @@ function addItem() {
   // console.log(arrayOfElements);
 }
 
-function loadP() {
-  console.log("ify", initialState, store.getState().length);
-}
+function loadP() {}
